9feee4a44f7ae3ba2876bb70aab6208b7c5d0b2b
Augusto Ramirez <augedsa@gmail.com>
2016-06-09 10:54:32 -0400

    database

---
+++ Database/hi5.sql
@@ -0,0 +1,96 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-06-2016 a las 16:53:59
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 5.6.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `hi5`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `friend`
+--
+
+CREATE TABLE `friend` (
+  `id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `friend`
+--
+
+INSERT INTO `friend` (`id`, `friend_id`, `user_id`) VALUES
+(7, 7, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `picture` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `picture`) VALUES
+(4, 'Andrew', 'augedsa@gmail.com', 'https://s.gravatar.com/avatar/8da1ac1f157ea754cf39ec5081b666ac?s=480'),
+(6, 'Arcadio', 'arcadio.perniac@gmail.com', 'https://s.gravatar.com/avatar/46687b2521ead150e6bf6cf272ba2eee?s=480'),
+(7, 'Jesus Sanchez', 'jesusgsancheze@gmail.com', 'https://s.gravatar.com/avatar/84c45f34813bff3bf46630bcae61f614?s=480');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `friend`
+--
+ALTER TABLE `friend`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `friend`
+--
+ALTER TABLE `friend`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT de la tabla `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
