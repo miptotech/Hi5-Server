e091245744b75f4a7d6bafa6ac19e3290b585f2d
Augusto Ramirez <augedsa@gmail.com>
2016-06-14 17:01:07 -0400

    database

--- Database/hi5.sql
+++ Database/hi5.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-06-2016 a las 01:41:44
--- Versión del servidor: 10.1.13-MariaDB
--- Versión de PHP: 5.6.20
+-- Tiempo de generación: 13-06-2016 a las 17:49:39
+-- Versión del servidor: 5.6.17
+-- Versión de PHP: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de datos: `hi5`
@@ -26,18 +26,12 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `friend`
 --
 
-CREATE TABLE `friend` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `friend` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `friend_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `friend`
---
-
-INSERT INTO `friend` (`id`, `friend_id`, `user_id`) VALUES
-(7, 7, 4);
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
 -- --------------------------------------------------------
 
@@ -45,14 +39,15 @@ INSERT INTO `friend` (`id`, `friend_id`, `user_id`) VALUES
 -- Estructura de tabla para la tabla `post`
 --
 
-CREATE TABLE `post` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `post` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `friend_id` int(11) NOT NULL,
   `message` varchar(300) NOT NULL,
   `picture` varchar(150) DEFAULT NULL,
-  `hi5` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `hi5` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -60,63 +55,14 @@ CREATE TABLE `post` (
 -- Estructura de tabla para la tabla `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(60) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `picture` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `picture` varchar(300) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
 
---
--- Volcado de datos para la tabla `user`
---
-
-INSERT INTO `user` (`id`, `name`, `email`, `picture`) VALUES
-(4, 'Augusto Ramirez', 'augedsa@gmail.com', 'https://s.gravatar.com/avatar/8da1ac1f157ea754cf39ec5081b666ac?s=480'),
-(6, 'Arcadio', 'arcadio.perniac@gmail.com', 'https://s.gravatar.com/avatar/46687b2521ead150e6bf6cf272ba2eee?s=480'),
-(7, 'Jesus Sanchez', 'jesusgsancheze@gmail.com', 'https://s.gravatar.com/avatar/84c45f34813bff3bf46630bcae61f614?s=480');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `friend`
---
-ALTER TABLE `friend`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `post`
---
-ALTER TABLE `post`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `friend`
---
-ALTER TABLE `friend`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT de la tabla `post`
---
-ALTER TABLE `post`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
